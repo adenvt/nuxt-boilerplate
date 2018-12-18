6bc4e0e0b70114b73d49e47c90b170c2ea7d275c
Ade Novid <adenvt@gmail.com>
2018-12-18 13:20:28 +0700

    [added]: nuxt + bootstrap

---
+++ store/index.js
@@ -0,0 +1,9 @@
+import EasyAccess, { defaultMutations } from 'vuex-easy-access'
+
+export const state = () => {
+  return { overlay: false }
+}
+
+export const mutations = { ...defaultMutations(state()) }
+
+export const plugins = [ EasyAccess() ]
