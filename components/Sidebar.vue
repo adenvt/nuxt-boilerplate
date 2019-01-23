ab276a8091b2834f18eecc354d13c2d1d59b3bdf
fajarekos <fajarekosusilo10@gmail.com>
2019-01-23 13:05:16 +0700

    create sidebar and navbar for dashboard

---
+++ components/Sidebar.vue
@@ -0,0 +1,98 @@
+<template>
+  <aside class="aside-container">
+    <nav class="sidebar">
+      <b-navbar-nav class="sidebar-nav">
+        <div class="nav-heading">
+          <span>Main Navigation</span>
+        </div>
+        <b-nav-item
+          v-b-toggle="'collapse1'"
+          href="#">
+          <i class="icon-speedometer"/>
+          <span>Dashboard</span>
+
+          <b-collapse id="collapse1">
+            <b-navbar-nav class="nav-child">
+              <b-nav-item href="#">
+                <span>Dashboard 1</span>
+              </b-nav-item>
+              <b-nav-item href="#">
+                <span>Dashboard 2</span>
+              </b-nav-item>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-nav-item>
+        <b-nav-item href="#">
+          <i class="icon-grid"/>
+          <span>Widgets</span>
+        </b-nav-item>
+        <b-nav-item href="#">
+          <i class="icon-layers"/>
+          <span>Layouts</span>
+        </b-nav-item>
+
+        <div class="nav-heading">
+          <span>Components</span>
+        </div>
+
+        <b-nav-item
+          v-b-toggle="'collapse2'"
+          aria-controls="collapse2"
+          href="#">
+          <i class="icon-chemistry"/>
+          <span>Elements</span>
+
+          <b-collapse id="collapse2">
+            <b-navbar-nav class="nav-child">
+              <b-nav-item href="#">
+                <span>Button</span>
+              </b-nav-item>
+              <b-nav-item href="#">
+                <span>Notification</span>
+              </b-nav-item>
+              <b-nav-item href="#">
+                <span>Carousel</span>
+              </b-nav-item>
+              <b-nav-item href="#">
+                <span>Sweet Alert</span>
+              </b-nav-item>
+              <b-nav-item href="#">
+                <span>Dropdown</span>
+              </b-nav-item>
+              <b-nav-item href="#">
+                <span>Cards</span>
+              </b-nav-item>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-nav-item>
+
+        <b-nav-item
+          v-b-toggle="'collapse3'"
+          aria-controls="collapse3"
+          href="#">
+          <i class="icon-note"/>
+          <span>Forms</span>
+
+          <b-collapse id="collapse3">
+            <b-navbar-nav class="nav-child">
+              <b-nav-item href="#">
+                <span>Standart</span>
+              </b-nav-item>
+              <b-nav-item href="#">
+                <span>Extended</span>
+              </b-nav-item>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-nav-item>
+
+      </b-navbar-nav>
+    </nav>
+  </aside>
+</template>
+<script>
+export default {
+  data () {
+    return { component: false }
+  },
+}
+</script>
